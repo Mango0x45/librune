89dc6b29ed9f9e25742b05234b2afddc7fe089ac
Thomas Voss <mail@thomasvoss.com>
2024-01-15 19:19:13 +0100

    Default to using memset() when input is ASCII

--- lib/utf8/u8set.c
+++ lib/utf8/u8set.c
@@ -7,9 +7,12 @@
 const char8_t *
 u8set(const char8_t *s, rune ch, size_t n)
 {
+	int m;
 	char8_t buf[U8_LEN_MAX];
-	int m = rtou8(buf, ch, sizeof(buf));
 
+	if (ch <= _1B_MAX)
+		return memset((char *)s, ch, n);
+	m = rtou8(buf, ch, sizeof(buf));
 	if (n % m != 0)
 		unreachable();
 	for (size_t i = 0; i < n; i += m)
