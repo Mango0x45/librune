f272268a70dffb7e45dcff5c4f8a4f562ed7153b
Thomas Voss <mail@thomasvoss.com>
2024-01-15 23:00:45 +0100

    Make u8gbrk_next() take a pointer to the length
    This keeps it compatible with the u8next() function.

--- lib/gbrk/u8gbrk_next.c
+++ lib/gbrk/u8gbrk_next.c
@@ -22,23 +22,26 @@ static bool u8isgbrk(rune, rune, struct gbrk_state *);
 static gbrk_prop getprop(rune);
 
 const char8_t *
-u8gbrk_next(const char8_t *s, size_t n)
+u8gbrk_next(const char8_t *s, size_t *n)
 {
 	int m;
 	rune ch1;
 	const char8_t *p = s;
 	struct gbrk_state gs = {0};
 
-	for (p += u8tor(&ch1, p); (size_t)(p - s) < n; p += m) {
+	for (p += u8tor(&ch1, p); (size_t)(p - s) < *n; p += m) {
 		rune ch2;
 
 		m = u8tor(&ch2, p);
-		if (u8isgbrk(ch1, ch2, &gs))
+		if (u8isgbrk(ch1, ch2, &gs)) {
+			*n -= p - s;
 			return p;
+		}
 
 		ch1 = ch2;
 	}
 
+	*n = 0;
 	return nullptr;
 }
 
