2446d2991e29fe60802a7bade101a6e77c8c1322
Thomas Voss <mail@thomasvoss.com>
2024-01-21 20:50:20 +0100

    Fix again

--- include/internal/types.h
+++ include/internal/types.h
@@ -1,5 +1,12 @@
-#if _RUNE_NEEDS_U8VIEW
+/* IWYU pragma: private */
 
+#include <stddef.h> /* IWYU pragma: export */
+#include <stdint.h>
+
+typedef unsigned char char8_t;
+typedef uint_least32_t rune;
+
+#if _RUNE_NEEDS_U8VIEW
 #	if !_RUNE_HAS_U8VIEW
 struct u8view {
 	const char8_t *p;
@@ -7,18 +14,4 @@ struct u8view {
 };
 #		define _RUNE_HAS_U8VIEW 1
 #	endif
-
 #endif
-
-#ifndef RUNE_INTERNAL_TYPES_H
-#	define RUNE_INTERNAL_TYPES_H
-
-/* IWYU pragma: private */
-
-#	include <stddef.h> /* IWYU pragma: export */
-#	include <stdint.h>
-
-typedef unsigned char char8_t;
-typedef uint_least32_t rune;
-
-#endif /* !RUNE_INTERNAL_TYPES_H */
