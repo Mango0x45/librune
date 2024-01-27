19e14b79ea10b26dad2bc6d78e93d286baa02a2d
Thomas Voss <mail@thomasvoss.com>
2024-01-27 15:07:34 +0100

    Fix broken u8strinit() impl

--- lib/builder/u8strinit.c
+++ lib/builder/u8strinit.c
@@ -4,12 +4,13 @@
 
 #include "internal/common.h"
 
-struct u8buf *
-u8strinit(struct u8buf *b, size_t n)
+struct u8str *
+u8strinit(struct u8str *b, size_t n)
 {
-	if (n && !(b->p = malloc(n)))
-		return nullptr;
-	else
+	if (n) {
+		if (!(b->p = malloc(n)))
+			return nullptr;
+	} else
 		b->p = nullptr;
 	b->len = 0;
 	b->cap = n;
