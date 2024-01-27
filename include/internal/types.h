7b3afbab9942e4732fb293368edd94ac31d542c1
Thomas Voss <mail@thomasvoss.com>
2024-01-27 15:07:07 +0100

    Simplify macro usage

--- include/internal/types.h
+++ include/internal/types.h
@@ -6,12 +6,10 @@
 typedef unsigned char char8_t;
 typedef uint_least32_t rune;
 
-#if _RUNE_NEEDS_U8VIEW
-#	if !_RUNE_HAS_U8VIEW
+#if _RUNE_NEEDS_U8VIEW && !_RUNE_HAS_U8VIEW
 struct u8view {
 	const char8_t *p;
 	size_t len;
 };
-#		define _RUNE_HAS_U8VIEW 1
-#	endif
+#	define _RUNE_HAS_U8VIEW 1
 #endif
