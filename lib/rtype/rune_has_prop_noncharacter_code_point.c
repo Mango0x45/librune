2590af7dd1e51d7745f3972ad255c52cc94efba1
Thomas Voss <mail@thomasvoss.com>
2024-02-01 21:01:17 +0100

    Include rune.h and format

--- lib/rtype/rune_has_prop_noncharacter_code_point.c
+++ lib/rtype/rune_has_prop_noncharacter_code_point.c
@@ -1,7 +1,10 @@
 #include "rtype.h"
+#include "rune.h"
 
 bool
 rune_has_prop_noncharacter_code_point(rune ch)
 {
-	return (ch >= 0xFDD0 && ch <= 0xFDEF) || ((ch & 0xFFFE) == 0xFFFE && (ch & RUNE_C(0xFF0000)) <= RUNE_C(0x0F0000));
+	return (ch >= 0xFDD0 && ch <= 0xFDEF)
+	    || ((ch & 0xFFFE) == 0xFFFE
+	        && (ch & RUNE_C(0xFF0000)) <= RUNE_C(0x0F0000));
 }
