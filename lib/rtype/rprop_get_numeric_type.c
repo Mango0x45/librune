8017bacca464b44c2d0dab36159b7d1084f62d39
Thomas Voss <mail@thomasvoss.com>
2024-02-02 01:02:24 +0100

    Add rprop_get_numeric_type()

---
+++ lib/rtype/rprop_get_numeric_type.c
@@ -0,0 +1,16 @@
+#include "rtype.h"
+
+#include "internal/rtype/nt.h"
+
+#define DEFAULT      NT_NONE
+#define HAS_VALUE    1
+#define LATIN1_TABLE rtype_nt_lat1_tbl
+#define TABLE        rtype_nt_tbl
+#define TYPE         enum numeric_type_bf
+#include "internal/rtype/lookup-func.h"
+
+enum numeric_type_bf
+rprop_get_numeric_type(rune ch)
+{
+	return lookup(ch);
+}
