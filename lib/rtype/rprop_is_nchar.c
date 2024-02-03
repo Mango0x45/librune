909d059658c21b286a4133bb7d5b44801eefdde7
Thomas Voss <mail@thomasvoss.com>
2024-02-03 01:32:13 +0100

    Use RUNE_C() for constants

--- lib/rtype/rprop_is_nchar.c
+++ lib/rtype/rprop_is_nchar.c
@@ -4,7 +4,7 @@
 bool
 rprop_is_nchar(rune ch)
 {
-	return (ch >= 0xFDD0 && ch <= 0xFDEF)
-	    || ((ch & 0xFFFE) == 0xFFFE
+	return (ch >= RUNE_C(0xFDD0) && ch <= RUNE_C(0xFDEF))
+	    || ((ch & RUNE_C(0xFFFE)) == RUNE_C(0xFFFE)
 	        && (ch & RUNE_C(0xFF0000)) <= RUNE_C(0x0F0000));
 }
--- lib/rtype/rprop_is_ri.c
+++ lib/rtype/rprop_is_ri.c
@@ -1,7 +1,8 @@
 #include "rtype.h"
+#include "rune.h"
 
 bool
 rprop_is_ri(rune ch)
 {
-	return ch >= 0x1F1E6 && ch <= 0x1F1FF;
+	return ch >= RUNE_C(0x1F1E6) && ch <= RUNE_C(0x1F1FF);
 }
