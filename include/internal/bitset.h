8fc8221e4cda1d7f91b8272d8705202eb20917e8
Thomas Voss <mail@thomasvoss.com>
2024-02-27 00:49:58 +0100

    Wrap _BSNB() argument in parenthesis

--- include/internal/bitset.h
+++ include/internal/bitset.h
@@ -3,7 +3,7 @@
 
 #include <limits.h>
 
-#define _BSNB(a)    (sizeof(a[0]) * CHAR_BIT)
+#define _BSNB(a)    (sizeof(*(a)) * CHAR_BIT)
 #define BSCHK(a, i) ((a)[(i) / _BSNB(a)] & (1 << ((i) % _BSNB(a))))
 
 #endif /* !RUNE_INTERNAL_BITSET_H */
