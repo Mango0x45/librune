e153c178fd0f350c81b753f13f9afd99020ca4e8
Thomas Voss <mail@thomasvoss.com>
2024-02-26 23:28:32 +0100

    Add TODO

--- include/internal/types.h
+++ include/internal/types.h
@@ -6,6 +6,7 @@
 typedef unsigned char char8_t;
 typedef uint_least32_t rune;
 
+/* TODO: Simplify this when GCC 14 drops (we can redefine this struct) */
 #if _RUNE_NEEDS_U8VIEW && !_RUNE_HAS_U8VIEW
 struct u8view {
 	const char8_t *p;
