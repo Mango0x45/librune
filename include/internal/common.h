63fd8d0837d92458fee9d8d5fb6ab7e510fcf845
Thomas Voss <mail@thomasvoss.com>
2024-02-27 02:09:22 +0100

    Fix spacing

--- include/internal/common.h
+++ include/internal/common.h
@@ -7,11 +7,11 @@
 
 #define lengthof(a) (sizeof(a) / sizeof(*(a)))
 
-#define U1(x) (((x)&0x80) == 0x00)
-#define U2(x) (((x)&0xE0) == 0xC0)
-#define U3(x) (((x)&0xF0) == 0xE0)
-#define U4(x) (((x)&0xF8) == 0xF0)
-#define UC(x) (((x)&0xC0) == 0x80)
+#define U1(x) (((x) & 0x80) == 0x00)
+#define U2(x) (((x) & 0xE0) == 0xC0)
+#define U3(x) (((x) & 0xF0) == 0xE0)
+#define U4(x) (((x) & 0xF8) == 0xF0)
+#define UC(x) (((x) & 0xC0) == 0x80)
 
 /* Maximum value of a 1–4-byte long UTF-8 sequence */
 #include "rune.h"
